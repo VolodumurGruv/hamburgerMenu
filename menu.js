ebe25aa9cc1b7089f727d4ab015cbee868580843
VolodumurGruv <gru.v@i.ua>
2021-02-24 01:21:25 +0200

    template is ready

--- menu.js
+++ menu.js
@@ -1,1 +1,17 @@
 "use strict";
+const menu = document.querySelector("#menu");
+const container = document.querySelector(".container");
+
+document.querySelector(".c-gamburger").addEventListener("click", function (e) {
+  e.preventDefault();
+  //   this.classList.toggle("is-active");
+  if (this.classList.contains("is-active")) {
+    this.classList.remove("is-active");
+    menu.classList.remove("nav-active");
+    container.classList.remove("body-active");
+  } else {
+    this.classList.add("is-active");
+    menu.classList.add("nav-active");
+    container.classList.add("body-active");
+  }
+});
